016868cb45f97080a6375be0b7941cdd86648b13
Orkhan Ahmadov <ahmadov90@gmail.com>
2020-01-20 15:18:21 +0100

    WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
     <vue-component></vue-component>
+
+    @livewire('todo')
 @endsection
--- resources/views/livewire/todo.blade.php
+++ resources/views/livewire/todo.blade.php
@@ -1,3 +1,3 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    If you look to others for fulfillment, you will never truly be fulfilled.
 </div>
